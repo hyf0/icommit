254faebfbe59b637fe1c94e3cac5b575853522a6
YunfeiHe <i.heyunfei@gmail.com>
2023-03-08 22:51:02 +0800

    "feat: Add new feature for better performance and user experience"

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,51 @@
-use std::{error::Error, fmt::format};
-use ansi_term::Color;
+use std::{borrow::Cow, process::Command};
 
+use ansi_term::Color;
+use hyper_tls::HttpsConnector;
+
+
+use anyhow::Result;
 use dialoguer::Confirm;
 use http::Request;
 
 use hyper::{Body, Client};
 use serde_json::{json, Value};
+use xshell::{Shell, cmd};
+
+
+fn staged_files(sh: &Shell) -> Result<Vec<String>> {
+    let output = cmd!(&sh, "git diff --name-only --cached").output()?;
+    assert!(output.status.success());
+    let files = String::from_utf8(output.stdout)?;
+    Ok(files.lines().into_iter().map(|s| s.to_string()).collect())
+}
+
+type StaticStr = Cow<'static, str>;
+
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let token = "Yeah. Should be a token here";
+async fn main() -> Result<()> {
+    let sh = Shell::new()?;
+
+    let verbose = true;
+
+    let staged_files = staged_files(&sh)?;
+
+    if staged_files.is_empty() {
+        Err(anyhow::format_err!("No staged files founded"))?
+    }
+
+    let token = std::env::var("ICOMMIT_TOKEN")?;
     let args = std::env::args().collect::<Vec<_>>();
     let msg_hint = args.get(1);
-    use hyper_tls::HttpsConnector;
+
+
+
+    let files_prompt: Vec<StaticStr> = vec![
+        "Consider changed files listed below".into(),
+        staged_files.into_iter().map(|file| format!("- {file}")).collect::<Vec<_>>().join("\n").into(),
+    ];
+
     let mut prompt = format!(
         "
 Generate a message of git commit with obeying following descriptions.
@@ -27,20 +60,20 @@ Obey following listed rules.
 "
     );
 
+    prompt += &files_prompt.join("\n");
+
     if let Some(msg_hint) = msg_hint {
         prompt.push_str(&format!(
             "
 Consider the hints: {msg_hint}
-
-Consider changed files listed below
-
-- Cargo.toml
-- main.rs
 "
         ))
     }
 
-    println!("prompt: {prompt}");
+    if verbose {
+        println!("prompt:{}", prompt);
+    }
+
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
 
@@ -71,7 +104,9 @@ Consider changed files listed below
     // Concatenate the body stream into a single buffer...
     let buf = hyper::body::to_bytes(res).await?;
     let buf = String::from_utf8(buf.to_vec()).unwrap();
-    println!("buf: {buf:?}");
+    if verbose {
+        println!("buf: {buf:?}");
+    }
     let v: Value = serde_json::from_str(&buf).unwrap();
 
     let commit_msg = v["choices"][0]["message"]["content"]
@@ -80,13 +115,16 @@ Consider changed files listed below
         .trim()
         .to_string();
     let command = format!(r#"git commit -m "{commit_msg}""#);
-    if Confirm::new().with_prompt(format!("Press enter to run `{}`", Color::Green.paint(command))).default(true).interact()? {
-        println!("Looks like you want to continue");
+
+    let git_commit_command_for_display = format!(r#"git commit -m "{}""#,  Color::Green.paint(&commit_msg));
+
+    if Confirm::new().with_prompt(git_commit_command_for_display).default(true).interact()? {
+        Command::new("git").arg("commit").arg(format!("-m \"{}\"", commit_msg)).output().unwrap();
     } else {
         println!("nevermind then :(");
     }
 
-    println!("commit_msg: {commit_msg}");
-
     Ok(())
 }
+
+
